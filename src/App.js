2b1c18975685ba3df6dd34803e0d9a230cb81e49
hamza411-ezzouggari <hamzahamaza256@gmail.com>
2021-12-22 10:43:36 +0200

    create new file for get value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import Result from "./Result";
 function App() {
   return (
     <div className="App">
       <h1>Hamza</h1>
+      <Result />
     </div>
   );
 }
---
+++ src/Result.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Result() {
+  return (
+    <div className="Result-finance">
+      <div className="title">
+        <h3>Vous pourriez être intéressé par ces actions</h3>
+      </div>
+      <div className="hr">
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default Result;

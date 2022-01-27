65ed35384d7e2c80890453f146794aaf46966d76
hamza411-ezzouggari <hamzahamaza256@gmail.com>
2022-01-27 11:07:17 +0200

    found solution

--- src/Tester.js
+++ src/Tester.js
@@ -1,45 +1,21 @@
-import React, { useEffect, useState, useCallback } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
+import React, { Component } from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function Tester() {
-  const [socketUrl, setSocketUrl] = useState("wss//localhost:4000");
-  const [messageHistory, setMessageHistory] = useState([]);
-  const handleClickChangeSocketUrl = useCallback(
-    () => setSocketUrl("wss//localhost:4000"),
-    []
-  );
+const client = new W3CWebSocket("ws://localhost:4000");
 
-  const handleClickSendMessage = useCallback(() => sendMessage("Hello"), []);
+class Test extends Component {
+  componentWillMount() {
+    client.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
 
-  useEffect(() => {
-    var lastMessage;
-    if (lastMessage !== null) {
-      setMessageHistory((prev) => prev.concat(lastMessage));
-    }
-  }, [lastMessage, setMessageHistory]);
-  const [sendMessage, lastMessage, readyState] = useWebSocket(socketUrl);
-
-  return (
-    <div>
-      <p>hamza</p>
-      <button onClick={handleClickChangeSocketUrl}>
-        Click Me to change Socket Url
-      </button>
-      <button
-        onClick={handleClickSendMessage}
-        disabled={readyState !== ReadyState.OPEN}
-      >
-        Click Me to send 'Hello'
-      </button>
-      <span>The WebSocket is currently </span>
-      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
-      <ul>
-        {messageHistory.map((message, idx) => (
-          <span key={idx}>{message ? message.data : null}</span>
-        ))}
-      </ul>
-    </div>
-  );
+  render() {
+    return <div>Practical Intro To WebSockets.</div>;
+  }
 }
 
-export default Tester;
+export default Test;

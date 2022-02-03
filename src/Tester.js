0576580c947cbfa10f737ba94ec85421c289491e
hamza411-ezzouggari <hamzahamaza256@gmail.com>
2022-02-03 08:07:50 +0200

    add auther tester

--- src/Tester.js
+++ src/Tester.js
@@ -1,50 +1,29 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
+import React, { Component } from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-const AppWs = () => {
-  const [isPaused, setIsPaused] = useState(false);
-  const [data, setData] = useState();
-  const ws = useRef(null);
-
-  useEffect(() => {
-    if (!isPaused) {
-      ws.current = new WebSocket("wss://ws.kraken.com/");
-      gettingData();
-    }
-    return () => ws.current.close();
-  }, [ws, isPaused]);
-
-  const gettingData = useCallback(() => {
-    if (!ws.current) return;
-
-    ws.current.onmessage = (e) => {
-      if (isPaused) return;
-      const message = JSON.parse(e.data);
-      setData(message);
+const client = new W3CWebSocket("http://localhost:4000/");
+var loc = window.location,
+  new_uri;
+if (loc.protocol === "https:") {
+  new_uri = "wss:";
+} else {
+  new_uri = "ws:";
+}
+new_uri += "//" + loc.host;
+new_uri += loc.pathname + "/to/ws";
+class App extends Component {
+  componentWillMount() {
+    client.onopen = () => {
+      console.log("WebSocket Client Connected");
     };
-  }, [isPaused]);
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
 
-  return (
-    <>
-      {!!data && (
-        <div>
-          <div>
-            <p>{`connection ID: ${data?.connectionID}`}</p>
-            <p>{`event: ${data?.event}`}</p>
-            <p>{`status: ${data?.status}`}</p>
-            <p>{`version: ${data?.version}`}</p>
-          </div>
-          <button
-            onClick={() => {
-              ws.current.close();
-              setIsPaused(!isPaused);
-            }}
-          >
-            {!isPaused ? "Stop connection" : "open connection"}
-          </button>
-        </div>
-      )}
-    </>
-  );
-};
+  render() {
+    return <div>Practical Intro To WebSockets.</div>;
+  }
+}
 
-export default AppWs;
+export default App;
